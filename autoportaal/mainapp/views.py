f3abda487751f9aebdf99a450772e1118e475f6d
Risto Vähi <fidtal0@gmail.com>
2018-10-14 15:08:35 +0300

    Update gitignore and move navbar into template

--- autoportaal/mainapp/views.py
+++ autoportaal/mainapp/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', {'nbar': 'index'})
 
 def login(request):
-    return render(request, 'mainapp/login.html')
+    return render(request, 'mainapp/login.html', {'nbar': 'login'})
 
 def uued(request):
-    return render(request, 'mainapp/uued.html')
-    
+    return render(request, 'mainapp/uued.html', {'nbar': 'uued'})
+
 def foorumid(request):
-    return render(request, 'mainapp/foorumid.html')
+    return render(request, 'mainapp/foorumid.html', {'nbar': 'foorumid'})
 
 def uudised(request):
-    return render(request, 'mainapp/uudised.html')
+    return render(request, 'mainapp/uudised.html', {'nbar': 'uudised'})
