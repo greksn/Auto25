c8d97caec8d2b6db508398dc5ca8d87da3b2fe8d
Magnar Pilvet <mpilvet@ut.ee>
2018-11-04 13:19:32 +0200

    added contact page

--- autoportaal/mainapp/urls.py
+++ autoportaal/mainapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('uued', views.uued, name='uued'),
     path('foorumid', views.foorumid, name='foorumid'),
     path('uudised', views.uudised, name='uudised'),
+    path('kontakt', views.kontakt, name='kontakt'),
     path('add_post', views.add_post, name="add_post"),
     path('language/eng', views.langEng, name="langEng"),
     path('language/est', views.langEst, name="langEst")
