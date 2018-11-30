9abe05cf4120b5129ce0933ea43a50a9cba4bf67
greor <gregor.noges@gmail.com>
2018-11-30 18:06:46 +0200

    offline web

--- autoportaal/mainapp/static/js/main.js
+++ autoportaal/mainapp/static/js/main.js
@@ -14,35 +14,8 @@ if (typeof jQuery == 'undefined') {
 }
 
 function renderAds() {
-    if (navigator.onLine) {
-        console.log("im online");
-        return;
+    if (!navigator.onLine) {
+        const text = $('#id_text').val();
+        localStorage.setItem("Post", text);
     }
-    db.transaction(function(tx) {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS Ads(brand TEXT, owner TEXT, model TEXT, gear_box TEXT, fuel TEXT, price INTEGER, year INTEGER, pic VARBINARY)', 
-        []);
-      tx.executeSql('SELECT * FROM Ads', [], function(tx, rs) {
-        for(var i = 0; i < rs.rows.length; i++) {
-          renderAd(rs.rows[i]);
-        }
-      });
-    });
-  }
-  
-  function insertAd(brand, owner, model, gear_box, fuel, price, year, pic) {
-    db.transaction(function(tx) {
-      tx.executeSql('INSERT INTO Ads VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [ brand, owner, model, gear_box, fuel, price, year, pic ],
-        function(tx, rs) {
-        },
-        function(tx, error) {
-          reportError('sql', error.message);
-        });
-    });
-  }
-
-  function renderAd(row) {
-    // add Ad to the database
-  }
-  function reportError(source, message) {
-    console.log(message);
-  }
+}
