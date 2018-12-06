4f211a7c228bd2ff8c61d2cf09f6b3ccf3e334a5
greor <gregor.noges@gmail.com>
2018-12-06 18:32:43 +0200

    Heade praktikate järgimine fix

--- autoportaal/autoportaal/settings.py
+++ autoportaal/autoportaal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ce81a^j6dvehhwhj2k3!tfk++vlrv*zcz3!ba+oq0zwk9-jd#j'
+SECRET_KEY = os.environ.get("SECRET_KEY_VEEBIRAKENDUSED", ''),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
